e459ab95d491be3299c391d57f5d3d7c8015291b
Göktuğ Karanlık <karanlik.goktug@gmail.com>
2021-12-01 15:59:23 +0300

    Create systemreq.py

---
+++ systemreq.py
@@ -0,0 +1,32 @@
+import platform
+
+#İşletim Sistemini Öğrenme
+print(platform.system())
+
+#İşlemci Bilgisi
+print(platform.processor())
+
+#Nesil Bilgisi
+print(platform.release())
+
+#Python Versiyonu
+print(platform.python_version())
+
+#Mimari Bilgisi
+print(platform.architecture())
+
+#Makine Bilgisi
+print(platform.machine())
+
+#PC Bağlantı Adı
+print(platform.node())
+
+#Python Compiler
+print(platform.python_compiler())
+
+#Python Versiyon
+print(platform.python_branch())
+
+#Python Implementasyon
+print(platform.python_implementation())
+
